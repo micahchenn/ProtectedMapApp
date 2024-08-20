ee10dc83986b73d08938db9a45e787b2abcf20b3
micahchenn <micahchen@utexas.edu>
2024-08-19 22:51:22 -0500

    fix deck.gl

---
+++ types/deck.gl__react.d.ts
@@ -0,0 +1,6 @@
+declare module '@deck.gl/react' {
+    import { DeckProps } from '@deck.gl/core';
+    import { Component } from 'react';
+  
+    export class DeckGL extends Component<DeckProps> {}
+  }
